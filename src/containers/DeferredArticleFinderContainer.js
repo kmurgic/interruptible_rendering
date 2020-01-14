a3f12b809a3837afb12eadfcd2a2eb584c581acc
Keith Murgic <keith.murgic@brandpulse.io>
2020-01-14 07:54:11 -0600

    added useDeferredValue example

---
+++ src/containers/DeferredArticleFinderContainer.js
@@ -0,0 +1,38 @@
+import React, { useDeferredValue, useState } from 'react'
+import loremIpsumArticles from '../loremIpsumArticles';
+import useWindowWidth from '../hooks/useWindowWidth';
+import ArticleFinder from '../components/ArticleFinder';
+
+const ArticleFinderContainer = () => {
+  const [searchText, setSearchText] = useState('');
+  const deferredSearchText = useDeferredValue(
+    searchText,
+    { timeoutMs: 5000 },
+  );
+
+  //update article length when screensize changes
+  const windowWidth = useWindowWidth();
+  const articleLength = Math.floor(Math.pow(windowWidth, 1.5) / 35);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchText(value);
+  };
+
+  const filteredArticles = loremIpsumArticles.filter(
+    article => (
+      article.title.toLowerCase().includes(deferredSearchText.toLowerCase())
+    )
+  );
+
+  return (
+    <ArticleFinder
+      articles={filteredArticles}
+      articleLength={articleLength}
+      handleChange={handleChange}
+      searchText={searchText}
+    />
+  );
+};
+
+export default ArticleFinderContainer;
