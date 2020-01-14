86cf76978faf43b1631e230d5c77c69e6b7c53a7
Keith Murgic <keith.murgic@brandpulse.io>
2020-01-14 07:55:38 -0600

    refactored blocking Article Finder

--- src/containers/ArticleFinderContainer.js
+++ src/containers/ArticleFinderContainer.js
@@ -4,34 +4,31 @@ import useWindowWidth from '../hooks/useWindowWidth';
 import ArticleFinder from '../components/ArticleFinder';
 
 const ArticleFinderContainer = () => {
-  const [articles, setArticles] = useState(loremIpsumArticles);
   const [searchText, setSearchText] = useState('');
 
   //update article length when screensize changes
-  const windowWidth = useWindowWidth()
-  const articleLength = Math.floor(Math.pow(windowWidth, 1.5) / 35)
+  const windowWidth = useWindowWidth();
+  const articleLength = Math.floor(Math.pow(windowWidth, 1.5) / 35);
 
   const handleChange = (e) => {
     const { value } = e.target;
     setSearchText(value);
-    setArticles(
-      loremIpsumArticles.filter(
-        article => (
-          article.title.toLowerCase().includes(value.toLowerCase())
-        )
-      )
-    );
-  }
+  };
+
+  const filteredArticles = loremIpsumArticles.filter(
+    article => (
+      article.title.toLowerCase().includes(searchText.toLowerCase())
+    )
+  );
 
   return (
     <ArticleFinder
-      articles={articles}
+      articles={filteredArticles}
       articleLength={articleLength}
       handleChange={handleChange}
       searchText={searchText}
     />
-  )
-
-}
+  );
+};
 
 export default ArticleFinderContainer;
